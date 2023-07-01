73f9b3c42bf0075272d9c7d8a611f5130c66149b
onurguler <guleronurosman@gmail.com>
2023-07-02 01:07:49 +0300

    Added AutoMapper and MediatR

---
+++ src/Core/HR.LeaveManagement.Application/MappingProfiles/LeaveTypeProfile.cs
@@ -0,0 +1,11 @@
+using AutoMapper;
+
+namespace HR.LeaveManagement.Application.MappingProfiles;
+
+public class LeaveTypeProfile : Profile
+{
+    public LeaveTypeProfile()
+    {
+        
+    }
+}
